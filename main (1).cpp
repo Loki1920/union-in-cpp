bc3807637c1247a4153c3c08ce634f5a05152e25
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-04-15 22:38:04 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,17 @@
+//union in c++
+#include<iostream>
+using namespace std;
+
+union money{
+  int rice;
+  char car;
+  float pounds;
+  };
+  
+int main()
+{
+  union money m1;
+  m1.rice = 28;
+  cout<<m1.rice<<endl;
+  return 0;
+}
